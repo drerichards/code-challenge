b3f0b5eea420fe8cd0b5fbc67dba0efb80c8da01
Preston Smith <smith.preston2@gmail.com>
2018-01-27 13:52:43 -0500

    add home page component

---
+++ client/src/components/Home.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Home = props => (
+  <div className="app">
+    <div className="page__background page__home">
+      <div className="page__title title__home">
+        Zomato!! <br/> The App! 
+      </div>
+      <div className="page__slogan slogan___home">
+        Hungry 
+        <br/>
+        <br/> 
+        Click Below and we'll 
+        show you what's available
+      </div>
+      <div className="page__action button__nom">
+        NOM NOW!!
+      </div>
+    </div>
+  </div>
+)
+
+export default Home
