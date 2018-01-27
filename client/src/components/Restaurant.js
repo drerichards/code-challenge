7f7f413e332faf7df928afb3e4c8f5e72b5b8ba8
Preston Smith (Laptop) <PrestonII@users.noreply.github.com>
2018-01-26 23:03:40 -0500

    reached limit of calls I can make to Zomato API haha

--- client/src/components/Restaurant.js
+++ client/src/components/Restaurant.js
@@ -4,23 +4,47 @@ import PropTypes from 'prop-types';
 class Restaurant extends React.Component {
   constructor(props){
     super(props);
+    
     this.state = {
-      isLoading: false,
+      isLoaded: false,
+      showExpanded: false,
       data: [],
     }
-
+    this.minimalContent = null;
+    this.expandedContent = null;
+    this.showExpanded = false;
     this.address = this.props.location.address;
+    this.initialize = this.initialize.bind(this);
+    this.toggleMoreInfo = this.toggleMoreInfo.bind(this);
+
+    this.initialize();
   }
 
   componentDidMount() {
     this.setState({
-      data: <li onClick={this.toggleMoreInfo}>{this.props.name}</li>
+      data: this.showExpanded ? this.expandedContent : this.minimalContent,
+      isLoaded: true
     })
+  }
+
+  initialize() {
+    this.minimalContent = <li>{this.props.name}</li>;
+    this.expandedContent = 
+      <ul>
+        <li>{this.props.name}</li>
+        <li>{this.props.location.address}</li>
+        <li>{this.props.url}</li>
+        <li>{this.props.is_delivering_now ? 'GET FOOD NOW!' : 'Sry :( Not Right Now'}</li>
+        <li></li>
+      </ul>
   }
 
   toggleMoreInfo(e) {
     e.preventDefault();
     console.log("this should show more info")
+    this.setState({
+      showExpanded : !this.showExpanded
+    })
   }
 
   render() {
@@ -28,7 +52,7 @@ class Restaurant extends React.Component {
         ? 'Loading...' 
         : this.state.data;
     return (
-      <ul>
+      <ul onClick={this.toggleMoreInfo}>
         { content }
       </ul>
     )
